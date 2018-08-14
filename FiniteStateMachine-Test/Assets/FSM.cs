d3805cdc254eae5adcc42745329d076f391879be
Aniluj <julianvolonterio@gmail.com>
2018-08-13 22:09:26 -0300

    Commit inicial
    Commit con una versión no funcional de una pequeña maquina de estados.

---
+++ FiniteStateMachine-Test/Assets/FSM.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM
+{
+    private int[,] fsm;
+    private int state;
+    private int initialState = 0;
+
+    public FSM(int statesCount, int eventsCount)
+    {
+        for (int i = 0; i < eventsCount; i++)
+        {
+            for (int j = 0; j < eventsCount; j++)
+            {
+                fsm[i, j] = -1;
+            }
+        }
+        state = initialState;
+    }
+
+    public void SetRelation(int srcState, int eventId, int destinationState)
+    {
+        fsm[srcState, eventId] = destinationState;
+    }
+
+    public int GetState()
+    {
+        return state;
+    }
+
+    public void SetEvent(int eventId)
+    {
+        if (fsm[state, eventId] != -1)
+        {
+            state = fsm[state, eventId];
+        }
+    }
+}
---
+++ FiniteStateMachine-Test/Assets/FSMTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSMTest : MonoBehaviour {
+
+    FSM fineStateMachine = new FSM(4, 1);
+    const int moveRight = 0;
+    const int moveUp = 1;
+    const int moveLeft = 2;
+    const int moveDown = 3;
+    Transform playerTransfor;
+    int changeDirectionMovement = 1;
+
+    void Start () {
+        
+    }
+	
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            changeDirectionMovement = 1;
+            fineStateMachine.SetEvent(changeDirectionMovement);
+        }
+
+        switch (fineStateMachine.GetState())
+        {
+            case moveRight :
+                transform.Translate(Vector3 right(1, 0, 0));
+                break;
+            case moveLeft:
+                Debug.Log("fsafsa");
+                break;
+        }
+	}
+}
