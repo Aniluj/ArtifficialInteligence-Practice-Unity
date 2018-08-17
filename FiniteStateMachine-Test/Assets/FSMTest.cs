a75fdb389da15d91491138940098862d78cafff4
Aniluj <julianvolonterio@gmail.com>
2018-08-17 04:50:07 -0300

    Funcionalidades de minero implementadas
    La maquina de estados ahora funciona para simular el funcionamiento de un minero que debe ir a la mina, extraer oro y depositarlo en una casa, recipiente o el lugar deseado.

--- FiniteStateMachine-Test/Assets/FSMTest.cs
+++ FiniteStateMachine-Test/Assets/FSMTest.cs
@@ -2,48 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FSMTest : MonoBehaviour {
-
-    private FSM finiteStatetMachine = new FSM(4, 1);
-    private const int moveRight = 0;
-    private const int moveUp = 1;
-    private const int moveLeft = 2;
-    private const int moveDown = 3;
-    private int changeDirection = 0;
-    private int movementState = 0;
+public class FSMTest : MonoBehaviour
+{
+    private const int goToTheMine = 0;
+    private const int goToTheHouse = 1;
+    private const int collectGold = 2;
+    private const int leaveGold = 3;
+    private const int stayStill = 4;
+    private int goldLeft = 0;
+    private int goldCollected = 1;
+    private int positionReached = 2;
+    private FSM finiteStatetMachine = new FSM(5, 3);
+    private Vector3 direction;
+    private Transform goldTransform;
+    public Transform goldCarryingPoint;
+    public Transform houseTransform;
+    public Transform mineTransform;
     public float velocity = 0;
 
-    void Start () {
-        
+    void Start ()
+    {
+        direction.Set(mineTransform.position.x - transform.position.x, 0, mineTransform.position.z - transform.position.z);
     }
 	
-	void Update () {
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            changeDirection = 0;
-            movementState += 1;
-            if (movementState == 4)
-            {
-                movementState = 0;
-            }
-            finiteStatetMachine.SetEvent(changeDirection);
-            finiteStatetMachine.SetRelation(finiteStatetMachine.GetState(), changeDirection, movementState);
-        }
-
+	void Update ()
+    {
         switch (finiteStatetMachine.GetState())
         {
-            case moveRight :
-                transform.Translate(Vector3.right * Time.deltaTime * velocity);
+            case goToTheMine :
+                transform.Translate((direction/direction.magnitude) * Time.deltaTime * velocity);
+                if ((this.transform.position.x >= mineTransform.position.x))
+                {
+                    finiteStatetMachine.SetRelation(finiteStatetMachine.GetState(), positionReached, collectGold);
+                    finiteStatetMachine.SetEvent(positionReached);
+                }
                 break;
-            case moveLeft:
-                transform.Translate(Vector3.left * Time.deltaTime * velocity);
+            case collectGold:
+                if (mineTransform.childCount != 0)
+                {
+                    mineTransform.Find("Gold").transform.SetParent(transform, true);
+                    transform.Find("Gold").transform.localPosition = goldCarryingPoint.localPosition;
+                    finiteStatetMachine.SetRelation(collectGold, goldCollected, goToTheHouse);
+                    finiteStatetMachine.SetEvent(goldCollected);
+                    direction.Set(houseTransform.position.x - transform.position.x, 0, houseTransform.position.z - transform.position.z);
+                }
+                else
+                {
+                    finiteStatetMachine.SetRelation(collectGold, goldCollected, stayStill);
+                    finiteStatetMachine.SetEvent(goldCollected);
+                }
                 break;
-            case moveUp:
-                transform.Translate(Vector3.up * Time.deltaTime * velocity);
+            case goToTheHouse:
+                transform.Translate((direction / direction.magnitude) * Time.deltaTime * velocity);
+                if (transform.position.x <= houseTransform.position.x)
+                {
+
+                    finiteStatetMachine.SetRelation(goToTheHouse, positionReached, leaveGold);
+                    finiteStatetMachine.SetEvent(positionReached);
+                }
                 break;
-            case moveDown:
-                transform.Translate(Vector3.down * Time.deltaTime * velocity);
+            case leaveGold:
+                transform.Find("Gold").transform.SetParent(houseTransform);
+                finiteStatetMachine.SetRelation(leaveGold, goldLeft, goToTheMine);
+                finiteStatetMachine.SetEvent(goldLeft);
+                direction.Set(mineTransform.position.x - transform.position.x, 0, mineTransform.position.z - transform.position.z);
+                break;
+            case stayStill:
+                velocity = 0;
                 break;
         }
 	}
