17237e429b1c090e2b6f94780e34a78bddc04752
Aniluj <julianvolonterio@gmail.com>
2018-09-10 21:54:41 -0300

    Creación de funciones y clases para BFS
    Se crearon las clases que por el momento considero necesarias para poder lograr lo necesario para conseguir un pathfinding de tipo BFS.

---
+++ FiniteStateMachine-Test/Assets/Scripts/Node.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    public List<Node> adjacents;
+    public float cost;
+    public float totalCost;
+    public bool isOpen;
+    public bool isClosed;
+    public Node parent;
+	
+	void Start () {
+        isOpen = false;
+        isClosed = false;
+	}
+	
+	
+	void Update () {
+		
+	}
+}
---
+++ FiniteStateMachine-Test/Assets/Scripts/PathFinderBFS.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinderBFS : MonoBehaviour {
+
+    Node goalNode;
+    Node originNode;
+    Node currentNode;
+    List<Vector3> pathToGoal = new List<Vector3>();
+    List<Node> openedNodes = new List<Node>();
+    List<Node> closedNodes = new List<Node>();
+
+    private void Awake()
+    {
+        
+    }
+
+    void Start ()
+    {
+        currentNode = originNode;
+	}
+	
+
+	void Update ()
+    {
+		
+	}
+
+    void BFS()
+    {
+        while(openedNodes.Count != 0)
+        {
+            currentNode.isClosed = true;
+            pathToGoal.Add(currentNode.transform.position);
+            closedNodes.Add(currentNode);
+            for(int i = 0; i < currentNode.adjacents.Count; i++)
+            {
+                if(currentNode.adjacents[i].isOpen == false && currentNode.adjacents[i].isClosed == false)
+                {
+                    currentNode.adjacents[i].isOpen = true;
+                    openedNodes.Add(currentNode.adjacents[i]);
+                }
+            }
+            for(int i=0; i<currentNode.adjacents.Count;i++)
+            {
+                if(currentNode.adjacents[i].isOpen == false && currentNode.adjacents[i].isClosed == false)
+                { 
+                    currentNode.adjacents[i].isOpen = true;
+                    currentNode.adjacents[i].parent = currentNode;
+                    openedNodes.Add(currentNode.adjacents[i]);
+                }
+                if(currentNode.adjacents[i].isOpen == true)
+                {
+                    currentNode = currentNode.adjacents[i];
+                    break;
+                }
+            }
+        }
+    }
+}
