06aa3a28b8ea72afd70da03cc7ec77fbe2ca20c1
Aniluj <julianvolonterio@gmail.com>
2018-09-18 06:00:49 -0300

    Pathfinding conseguido
    Se consigue encontrar el camino y mover el cubo hasta la posición que se desea encontrar, aunque aun no sé si lo hago de la forma correcta.

--- FiniteStateMachine-Test/Assets/Scripts/PathFinderBFS.cs
+++ FiniteStateMachine-Test/Assets/Scripts/PathFinderBFS.cs
@@ -7,7 +7,11 @@ public class PathFinderBFS : MonoBehaviour {
 
     public Node goalNode;
     public Node originNode;
+    public GameObject agent;
+    public float movementSpeed;
     private Node currentNode;
+    private bool goalFound;
+    private int i;
     List<Vector3> pathToGoal = new List<Vector3>();
     List<Node> openedNodes = new List<Node>();
     List<Node> closedNodes = new List<Node>();
@@ -20,63 +24,59 @@ public class PathFinderBFS : MonoBehaviour {
     void Start ()
     {
         currentNode = originNode;
+        i = 0;
         BFS();
 	}
 	
 
 	void Update ()
     {
-        
+        i = pathToGoal.Count-1;
+        if(goalFound)
+        {
+            agent.transform.localPosition = Vector3.MoveTowards(agent.transform.position, pathToGoal[i], movementSpeed * Time.deltaTime);
+            if(agent.transform.position == pathToGoal[i])
+            {
+                i--;
+            }
+            if(i<0)
+            {
+                goalFound = false;
+            }
+        }
 	}
 
     void BFS()
     {
-        if (currentNode == originNode)
-        {
-            pathToGoal.Add(currentNode.transform.position);
-            closedNodes.Add(currentNode);
-        }
-        for (int i = 0; i < currentNode.adjacents.Count; i++)
-        {
-            if (currentNode.adjacents[i].isOpen == false && currentNode.adjacents[i].isClosed == false)
-            {
-                currentNode.adjacents[i].isOpen = true;
-                openedNodes.Add(currentNode.adjacents[i]);
-            }
-        }
+        currentNode.isOpen = true;
+        openedNodes.Add(currentNode);
+        currentNode.parent = originNode;
         while (openedNodes.Count != 0)
         {
+            currentNode.isOpen = false;
             currentNode.isClosed = true;
-            if(currentNode.parent != null)
-            {
-                Debug.Log(currentNode.parent.transform.position);
-                pathToGoal.Add(currentNode.parent.transform.position);
-            }
+            openedNodes.Remove(currentNode);
+            closedNodes.Add(currentNode);
+            pathToGoal.Add(currentNode.parent.transform.position);
             if(currentNode == goalNode)
             {
+                Debug.Log(currentNode.transform.localPosition);
+                pathToGoal.Add(currentNode.transform.position);
+                goalFound = true;
                 break;
             }
-            for (int i = 0; i < currentNode.adjacents.Count; i++)
+            for(int i=0; i<currentNode.adjacents.Count; i++)
             {
-                if(currentNode.adjacents[i].isOpen == false && currentNode.adjacents[i].isClosed == false)
+                if(currentNode.adjacents[i].isOpen != true && currentNode.adjacents[i].isClosed != true)
                 {
-                    currentNode.adjacents[i].isOpen = true;
-                    openedNodes.Add(currentNode.adjacents[i]);
-                }
-            }
-            for(int i=0; i<currentNode.adjacents.Count;i++)
-            {
-                if(currentNode.adjacents[i].isOpen == false && currentNode.adjacents[i].isClosed == false)
-                { 
                     currentNode.adjacents[i].isOpen = true;
                     currentNode.adjacents[i].parent = currentNode;
                     openedNodes.Add(currentNode.adjacents[i]);
                 }
-                if(currentNode.adjacents[i].isOpen == true)
-                {
-                    currentNode = currentNode.adjacents[i];
-                    break;
-                }
+            }
+            if(openedNodes.Count != 0)
+            {
+                currentNode = openedNodes[0];
             }
         }
     }
