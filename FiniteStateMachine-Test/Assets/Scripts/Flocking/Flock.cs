b7ca222d469c340f76697b3b6777bbc30bc64ed8
Aniluj <julianvolonterio@gmail.com>
2019-02-07 21:57:43 -0300

    Flocking y algoritmo de Path Finding agregado
    Se creó una escena que demuestra el uso correcto del Flocking, y se corrigió el código de Path Finding que ya existía para que haga la búsqueda correspondiente.

---
+++ FiniteStateMachine-Test/Assets/Scripts/Flocking/Flock.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour {
+
+    public FlockManager manager;
+    public float speed;
+
+    void Start() {
+        speed = Random.Range(manager.minSpeed, manager.maxSpeed);
+    }
+
+
+    void Update() {
+        ApplyRules();
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+
+    void ApplyRules(){
+        GameObject[] gos;
+        gos = manager.allFish;
+
+        Vector3 vcentre = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.01f;
+        float nDistance;
+        int groupSize = 0;
+
+        foreach(GameObject go in gos)
+        {
+            if(go != this.gameObject)
+            {
+                nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+                if(nDistance <= manager.neighbourDistance)
+                {
+                    vcentre += go.transform.position;
+                    groupSize++;
+
+                    if(nDistance < 1.0f)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+
+                    Flock anotherFlock = go.GetComponent<Flock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+                }
+            }
+        }
+
+        if(groupSize > 0)
+        {
+            vcentre = vcentre / groupSize + (manager.goalPos - this.transform.position);
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+            if(direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), manager.rotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
--- FiniteStateMachine-Test/Assets/Scripts/PathFinderBFS.cs
+++ FiniteStateMachine-Test/Assets/Scripts/PathFinderBFS.cs
@@ -12,36 +12,30 @@ public class PathFinderBFS : MonoBehaviour {
     private Node currentNode;
     private bool goalFound;
     private int i;
-    List<Vector3> pathToGoal = new List<Vector3>();
-    List<Node> openedNodes = new List<Node>();
+    Stack<Vector3> pathToGoal = new Stack<Vector3>();
+    Queue<Node> openedNodes = new Queue<Node>();
     List<Node> closedNodes = new List<Node>();
-
-    private void Awake()
-    {
-        
-    }
 
     void Start ()
     {
+        originNode.parent = null;
         currentNode = originNode;
         i = 0;
         BFS();
 	}
-	
 
 	void Update ()
     {
-        i = pathToGoal.Count-1;
-        if(goalFound)
+        if(goalFound == true)
         {
-            agent.transform.localPosition = Vector3.MoveTowards(agent.transform.position, pathToGoal[i], movementSpeed * Time.deltaTime);
-            if(agent.transform.position == pathToGoal[i])
+            GetPath();
+        }
+        if(pathToGoal.Count != 0)
+        {
+            agent.transform.position = Vector3.MoveTowards(agent.transform.position, pathToGoal.Peek(), movementSpeed * Time.deltaTime);
+            if(agent.transform.position == pathToGoal.Peek())
             {
-                i--;
-            }
-            if(i<0)
-            {
-                goalFound = false;
+                pathToGoal.Pop();
             }
         }
 	}
@@ -49,35 +43,44 @@ public class PathFinderBFS : MonoBehaviour {
     void BFS()
     {
         currentNode.isOpen = true;
-        openedNodes.Add(currentNode);
-        currentNode.parent = originNode;
-        while (openedNodes.Count != 0)
+        openedNodes.Enqueue(currentNode);
+        while(openedNodes.Count != 0)
         {
-            currentNode.isOpen = false;
-            currentNode.isClosed = true;
-            openedNodes.Remove(currentNode);
-            closedNodes.Add(currentNode);
-            pathToGoal.Add(currentNode.parent.transform.position);
-            if(currentNode == goalNode)
+            if(currentNode.transform.position == goalNode.transform.position)
             {
-                Debug.Log(currentNode.transform.localPosition);
-                pathToGoal.Add(currentNode.transform.position);
                 goalFound = true;
                 break;
             }
-            for(int i=0; i<currentNode.adjacents.Count; i++)
+            for(int i = 0; i < currentNode.adjacents.Count;i++)
             {
-                if(currentNode.adjacents[i].isOpen != true && currentNode.adjacents[i].isClosed != true)
+                if(currentNode.adjacents[i].isOpen == false && currentNode.adjacents[i].isClosed == false)
                 {
                     currentNode.adjacents[i].isOpen = true;
+                    openedNodes.Enqueue(currentNode.adjacents[i]);
                     currentNode.adjacents[i].parent = currentNode;
-                    openedNodes.Add(currentNode.adjacents[i]);
                 }
             }
+            openedNodes.Dequeue();
+            currentNode.isOpen = false;
+            currentNode.isClosed = true;
+            closedNodes.Add(currentNode);
             if(openedNodes.Count != 0)
             {
-                currentNode = openedNodes[0];
+                currentNode = openedNodes.Peek();
             }
+        }
+    }
+
+    void GetPath()
+    {
+        if(currentNode.transform.position != originNode.transform.position)
+        {
+            pathToGoal.Push(currentNode.transform.position);
+            currentNode = currentNode.parent;
+        }
+        else
+        {
+            goalFound = false;
         }
     }
 }
