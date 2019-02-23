e0432ac1e89336971dd32caa92ab8ed6c53958e7
Aniluj <julianvolonterio@gmail.com>
2019-02-23 05:08:49 -0300

    Dijkstra modificado
    Se modificó el funcionamiento de Dijkstra ya que el anterior si bien cumplía con encontrar el camino, el método no era realmente el de Dijkstra y en determinadas ocasiones estaba la posibilidad de no encontrar el camino en cuestión. Ahora funciona como debe, a excepción que al igual que en el commit anterior, el valor que se suma para generar el total cost, idealmente debería ser proporcionado por una fórmula diferente.

--- FiniteStateMachine-Test/Assets/Scripts/PathFinding/Dijkstra.cs
+++ FiniteStateMachine-Test/Assets/Scripts/PathFinding/Dijkstra.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Dijkstra {
 
-    int counter = 0;
+    /*int counter = 0;
     Node auxNode;
+    int secondCounter = 0;*/
 
     public void DijkstraAlgorithm(ref Node currentNode, ref List<Node> openedNodes, ref Node goalNode, ref bool goalFound) {
         currentNode.isOpen = true;
         while(openedNodes.Count != 0)
         {
-            counter = 0;
+            //counter = 0;
             if(currentNode.transform.position == goalNode.transform.position)
             {
                 goalFound = true;
@@ -21,13 +22,18 @@ public class Dijkstra {
             {
                 if(currentNode.adjacents[i].isOpen == false && currentNode.adjacents[i].isClosed == false)
                 {
+                    //Debug.Log(currentNode.name + ": " + currentNode.adjacents[i].name);
                     currentNode.adjacents[i].isOpen = true;
                     openedNodes.Add(currentNode.adjacents[i]);
                     currentNode.adjacents[i].totalCost = 1 + currentNode.adjacents[i].cost;
                     currentNode.adjacents[i].parent = currentNode;
-                    if(counter > 0)
+                    /*if(counter > 0)
                     {
                         if(currentNode.adjacents[i].totalCost < auxNode.totalCost)
+                        {
+                            auxNode = currentNode.adjacents[i];
+                        }
+                        else if (currentNode.adjacents[i].totalCost == auxNode.totalCost)
                         {
                             auxNode = currentNode.adjacents[i];
                         }
@@ -36,23 +42,32 @@ public class Dijkstra {
                     {
                         auxNode = currentNode.adjacents[i];
                     }
-                    counter++;
+                    counter++;*/
                 }
             }
-            openedNodes.RemoveAt(0);
-            openedNodes[0].isOpen = false;
-            openedNodes[0].isClosed = true;
-            Debug.Log(auxNode.name + ": " + auxNode.totalCost);
+            Debug.Log(currentNode.name + ": " + currentNode.totalCost);
+            
+            /*for(int i = 0; i < openedNodes.Count; i++)
+            {
+                Debug.Log("iteracion " + secondCounter + ":" + openedNodes[i].name);
+            }*/
+
+            //openedNodes.RemoveAt(0);
+            openedNodes.Remove(currentNode);
+            currentNode.isOpen = false;
+            currentNode.isClosed = true;
+            //Debug.Log(auxNode.name + ": " + auxNode.totalCost);
 
             if(openedNodes.Count != 0)
             {
-                if(openedNodes.Contains(auxNode))
+                currentNode = openedNodes[0];
+
+                foreach(Node auxnode in openedNodes)
                 {
-                    currentNode = auxNode;
-                }
-                else
-                {
-                    currentNode = openedNodes[0];
+                    if(auxnode.totalCost < currentNode.totalCost)
+                    {
+                        currentNode = auxnode;
+                    }
                 }
             }
             /*if(openedNodes.Count != 0)
